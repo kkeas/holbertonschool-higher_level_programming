d6560278e62d9514ad8b6568b8f51ea9f1363e13
kkeas <keaskatrina@gmail.com>
2023-01-07 19:28:30 +0000

    changed equation

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-last_digit = int(str(number)[-1])
+last_digit = number % 10
 
 if last_digit > 5:
     print("Last digit of", number, "is", last_digit, "and is greater than 5")
