b073dca836d55fdd282f2d6ebd66011b902e0743
kkeas <keaskatrina@gmail.com>
2023-01-11 03:41:21 +0000

    convert from low to upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    for char in str:
+        if ord(char) > 96 and ord(char) < 123:
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
