2ddb771c22c3f2a87b424daaa61d15bdedf03eec
kkeas <keaskatrina@gmail.com>
2023-01-11 15:46:03 +0000

    extra 99

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(0, 100):
-    print("{0:0=2d},".format(i), end=" ")
     if i == 99:
-        print("{}\n".format(i))
+        print("{0:0=2d}".format(i))
+    else:
+        print("{0:0=2d},".format(i), end=" ")
