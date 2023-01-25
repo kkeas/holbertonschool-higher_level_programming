16f1f4da4bf7a57efc8cbba91166f2ccf4ef7b1f
kkeas <keaskatrina@gmail.com>
2023-01-25 21:59:49 +0000

    done

---
+++ 0x04-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""The Square Module"""
+
+
+class Square:
+    """Defines a class named Square"""
+    def __init__(self, size):
+        """
+        Initialize the square object with a size
+        Args:
+            size (int): the size of one side of Square
+        """
+        self.__size = size
