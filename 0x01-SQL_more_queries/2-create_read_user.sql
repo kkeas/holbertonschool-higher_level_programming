40f97d4b9f632ab3e66fb3d69104d6d947fe677d
kkeas <keaskatrina@gmail.com>
2023-02-14 12:59:36 -0800

    remove flush

--- 0x01-SQL_more_queries/2-create_read_user.sql
+++ 0x01-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,4 @@
--- create
--- comment 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user with select
+CREATE DATABASE IF NOT EXISTS hbtn_0d_02;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_02.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
