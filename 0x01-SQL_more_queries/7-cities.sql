2bd2522a340b737aa589c35c30472c15218af34a
kkeas <keaskatrina@gmail.com>
2023-02-14 19:16:02 -0800

    cities

---
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- CITIES
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT AUTO_INCREMENT NOT NULL UNIQUE,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
