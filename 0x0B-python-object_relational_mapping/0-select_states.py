1a1fc11b10f9b5e2248edd7fa95ca6fb3ba974ee
kkeas <keaskatrina@gmail.com>
2023-02-28 10:52:36 -0800

    typos

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """comment"""
-
-import SQLdb
+import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    connection = SQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
+    connection = MySQLdb.connect(user=sys.argv[1], password=sys.argv[2], db=sys.argv[3])
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states")
     [print(state) for state in cursor.fetchall()]
