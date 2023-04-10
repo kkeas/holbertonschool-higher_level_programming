b78b1964962d18cba5718cbed9153b9aa2a9ceba
kkeas <keaskatrina@gmail.com>
2023-04-10 14:52:08 -0700

    pls

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 
 if (process.argv.length <= 2) {
     console.log("No arguments");
-  } else if (process.argv.length === 1) {
+  } else if (process.argv.length === 3) {
     console.log("Argument found");
   } else {
     console.log("Arguments found")
