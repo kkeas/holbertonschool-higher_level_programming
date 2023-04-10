da7ed2196afdba3663f404743979477ff1cb1226
kkeas <keaskatrina@gmail.com>
2023-04-10 13:27:52 -0700

    easy money

---
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const cisfun = 'C is fun';
+const piscool = 'Python is cool';
+const jisamazing = 'JavaScript is amazing';
+
+console.log(cisfun);
+console.log(piscool);
+console.log(jisamazing);
