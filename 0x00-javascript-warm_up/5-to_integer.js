12908471a8dc5e66deb41cc50a6755c68cc53a31
kkeas <keaskatrina@gmail.com>
2023-04-10 16:19:56 -0700

    singlequote

--- 0x00-javascript-warm_up/5-to_integer.js
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 
 const [arg1] = process.argv.slice(2);
-
 const num = parseInt(arg1, 10);
 
 if (isNaN(num)) {
-  console.log("Not a number");
+  console.log('Not a number');
 } else {
   console.log(`My number: ${num}`);
 }
