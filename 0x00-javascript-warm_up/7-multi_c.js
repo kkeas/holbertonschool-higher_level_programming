79ce1afb0889f4b84b1bb66ac29efaaec55f6bc5
kkeas <keaskatrina@gmail.com>
2023-04-11 09:53:19 -0700

    semistandard

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const numOccurrences = parseInt(process.argv[2], 10);
 
 if (isNaN(numOccurrences)) {
-  console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < numOccurrences; i++) {
-    console.log("C is fun");
+    console.log('C is fun');
   }
 }
