75adc0b2f0dfada98db5843a82a6d135818c12e2
kkeas <keaskatrina@gmail.com>
2023-04-11 10:13:36 -0700

    semi

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-function add(a, b) {
+function add (a, b) {
   a = parseInt(process.argv[2], 10);
   b = parseInt(process.argv[3], 10);
   const sum = a + b;
-    return console.log(sum)
+  return console.log(sum);
 }
 
 add();
