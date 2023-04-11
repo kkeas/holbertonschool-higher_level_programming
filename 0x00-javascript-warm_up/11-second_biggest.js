c1b90db280100ea7b0ceb4c81b665b7621422223
kkeas <keaskatrina@gmail.com>
2023-04-11 15:03:23 -0700

    work

---
+++ 0x00-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length === 0) {
+  console.log(0);
+} else if (args.length === 1) {
+  console.log(0);
+} else {
+  let max = args[0];
+  let secondMax = args[1];
+  
+  if (secondMax > max) {
+    [max, secondMax] = [secondMax, max];
+  }
+
+  for (let i = 2; i < args.length; i++) {
+    if (args[i] > max) {
+      secondMax = max;
+      max = args[i];
+    } else if (args[i] > secondMax) {
+      secondMax = args[i];
+    }
+  }
+  
+  console.log(secondMax);
+}
