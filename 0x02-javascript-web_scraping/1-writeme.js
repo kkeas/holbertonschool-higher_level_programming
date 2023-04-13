094266d9d08b6742e9eba13e8152b4df814596b6
kkeas <keaskatrina@gmail.com>
2023-04-13 13:06:52 -0700

    i did it

---
+++ 0x02-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+const buffer = Buffer.from(content, 'utf-8');
+
+fs.writeFile(filePath, content, (err) => {
+  if (err) throw err;
+  console.log(content);
+});
