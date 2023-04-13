8221f697ac100549b463b962173e5618c042e235
kkeas <keaskatrina@gmail.com>
2023-04-13 12:21:07 -0700

    semi

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf8', function(err, data) {
+fs.readFile(filePath, 'utf8', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
