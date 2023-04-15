0ca3cc00cd063d9ae9e749f01a721b1f85883d21
kkeas <keaskatrina@gmail.com>
2023-04-15 16:37:49 -0700

    back ticks

---
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const episode = parseInt(process.argv[2]);
+
+const url = `https://swapi-api.hbtn.io/api/films/${episode}`;
+
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    console.log(`${data.title}`);
+  } else {
+      console.error('Error:', error);
+  }
+});
