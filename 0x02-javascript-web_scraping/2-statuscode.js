4b18c5588ae67253c9b043d2e90cda2242351253
kkeas <keaskatrina@gmail.com>
2023-04-15 13:26:07 -0700

    check if status is null

--- 0x02-javascript-web_scraping/2-statuscode.js
+++ 0x02-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const url = process.argv[2];
 
-request(url, (response) => {
-  console.log(`code: ${response.statusCode}`);
+request(url, function (error, response, body) {
+  if (response && response.statusCode) {
+    console.log(`code: ${response.statusCode}`);
+  } else {
+    console.error('Error:', error);
+  }
 });
